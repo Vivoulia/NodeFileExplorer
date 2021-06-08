effefde61b4b53d3b9006cc8c0163142968f18b2
Batoch <baptiste.massardier@gmail.com>
2021-06-08 17:47:12 +0200

    add video

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var APIRouter = require("./routes/api");
 var filesRouter = require("./routes/filesrouter");
+var videoRouter = require("./routes/videoRouter");
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use("/api", APIRouter);
 app.use("/files", filesRouter);
+app.use("/video", videoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
