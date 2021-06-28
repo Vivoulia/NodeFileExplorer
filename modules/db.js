1e7cc7769c018558c07a473b6aa259ffa5f31805
Batoch <baptiste.massardier@gmail.com>
2021-06-28 14:55:14 +0200

    Working default user

--- modules/db.js
+++ modules/db.js
@@ -15,13 +15,6 @@ class Db {
                 password text,
                 admin boolean)`
         this.db.run(sql);
-
-        this.selectByName("admin", function(err, user) {
-            if(!user){
-                this.db.run(
-                    'INSERT INTO user (name,password, admin) VALUES (admin,ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff,1)')
-            }
-        });
     }
 
     selectByName(name, callback) {
