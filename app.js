c7b594d206b9a5e31dcdd1e471ed2c90d783ef37
Batoch <baptiste.massardier@gmail.com>
2021-06-28 15:26:09 +0200

    remove verbose

--- app.js
+++ app.js
@@ -63,7 +63,6 @@ passport.deserializeUser(function(id, cb) {
 
 // Create a default account if needed
 db.selectAll(function(err, user) {
-  console.log(user[0])
   if (!user[0]) { db.insert("admin", "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff", "1", function(err, user) {if (err) { console.log(err) }})}
 })
 
